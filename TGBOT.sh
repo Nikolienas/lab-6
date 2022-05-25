8345d2daf90fdba04c2e499d06e9744ccb867c9f
Nikolienas <94343153+Nikolienas@users.noreply.github.com>
2022-05-26 02:28:44 +0500

    Update TGBOT.sh

--- TGBOT.sh
+++ TGBOT.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+#Первая строка путь к интепретатору, вторая путь к файлу
 C:/Users/DD/PycharmProjects/testing_ground/venv/Scripts/python.exe C:/Users/DD/PycharmProjects/testing_ground/bot_v3.py
