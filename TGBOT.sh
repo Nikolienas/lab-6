ff5632b5205fcb105de9b2f3051138487439add9
Nikolienas <94343153+Nikolienas@users.noreply.github.com>
2022-05-31 11:08:56 +0500

    Delete TGBOT.sh

--- TGBOT.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#Первая строка путь к интепретатору, вторая путь к файлу
-C:/Users/DD/PycharmProjects/testing_ground/venv/Scripts/python.exe C:/Users/DD/PycharmProjects/testing_ground/bot_v3.py
